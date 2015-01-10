1a556940e2c4c5fb40df7abe83c5847e9365c7cc
sekulic <sekulic.radosav@yahoo.com>
2015-01-10 16:09:56 +0100

    add glas to table odgovor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108165415) do
+ActiveRecord::Schema.define(version: 20150110143535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20141108165415) do
     t.string   "sadrzaj"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "glas",       limit: 8, default: 0
   end
 
   add_index "odgovori", ["pitanje_id"], name: "index_odgovori_on_pitanje_id", using: :btree
